f12b991f2374b1aeab025a041721b95bb21abd28
ypexxx <yua.alvin@gmail.com>
2024-08-13 18:11:09 +0700

    update progress

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,1 +1,17 @@
-const test = console.log("test");
+//seleksi kelas menu
+const menu = document.querySelector(".menu");
+
+//ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+    menu.classList.toggle("active");
+}
+
+//klik dimana saja untuk menghilangkan navbar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !menu.contains(e.target)) {
+        menu.classList.remove("active");
+    }
+});
+
