b00b0431cbddb60fb1a22a5c30acdbb9920d921f
ypexxx <yua.alvin@gmail.com>
2024-08-23 00:34:44 +0700

    migrasi ke tailwindcss dan update progress

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
