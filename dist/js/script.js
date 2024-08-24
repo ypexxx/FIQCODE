e302fa48e98c5f2f0120a28c95ad3db89963243c
ypexxx <yua.alvin@gmail.com>
2024-08-25 01:08:48 +0700

    update progress

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,12 @@ const hamburger = document.querySelector('#hamburger');
 hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
 });
+
+// share button
+const link = encodeURI(window.document.location.href);
+const msg = encodeURIComponent('njajallll');
+
+const whatsapp = document.querySelector('#whatsapp-button');
+const aaa = whatsapp.href = 'https://api.whatsapp.com/send?text=${msg}%20${link}';
+
+console.log(link);
