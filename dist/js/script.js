f9a6197a0deb90881cc059e11c453dd00194fc78
ypexxx <yua.alvin@gmail.com>
2024-09-02 14:24:46 +0700

    update progress

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,12 +5,39 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
 });
 
-//adsterra link
-const button = document.querySelector('#ads-btn');
 
-button.addEventListener('click', function() {
-    window.open('https://www.cpmrevenuegate.com/fbmcre2rey?key=2d4a6c477a55837d9f6d4d43f79ca38f');
-    button.remove();
+// stream section
+const stream = document.querySelector('#stream');
+
+
+
+// schedule
+// schedule1
+const schedule1 = document.querySelector('#schedule1');
+schedule1.addEventListener('click', function() {
+    stream.setAttribute('src', 'https://channeltvvivasport.blogspot.com/2024/07/tnt-sport-1.html');
 });
 
-// share button
+// schedule2
+const schedule2 = document.querySelector('#schedule2');
+schedule2.addEventListener('click', function() {
+    stream.setAttribute('src', 'https://channeltvvivasport.blogspot.com/2024/08/tntsport2.html');
+});
+
+// schedule3
+const schedule3 = document.querySelector('#schedule3');
+schedule3.addEventListener('click', function() {
+    stream.setAttribute('src', 'https://live.sportsnews.id/p/eleven-sports-1-pt.html');
+});
+
+// schedule4
+const schedule4 = document.querySelector('#schedule4');
+schedule4.addEventListener('click', function() {
+    stream.setAttribute('src', 'https://channeltvvivasport.blogspot.com/2024/08/tntsport4.html');
+});
+
+// schedule5
+const schedule5 = document.querySelector('#schedule5');
+schedule5.addEventListener('click', function() {
+    stream.setAttribute('src', 'https://live.sportsnews.id/p/eleven-sports-2-pt.html');
+});
